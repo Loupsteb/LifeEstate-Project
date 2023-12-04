a0df1548cf0ba70848ee89087d894f1dcc36dbd1
Loup <esteban.loup@gmail.com>
2023-12-04 15:41:03 +0100

    :add natspect to the solidity contracts:

--- Backend/contracts/LifeEstateFactory.sol
+++ Backend/contracts/LifeEstateFactory.sol
@@ -4,16 +4,35 @@ pragma solidity ^0.8.20;
 import "./LifeEstateNFT.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/// @title A factory contract for creating and initializing LifeEstateNFT contracts.
+/// @notice This contract allows deployment and initialization of LifeEstateNFT instances.
 contract LifeEstateFactory is Ownable {
+    /// @notice Array of deployed LifeEstateNFT contract addresses.
     address[] public LifeEstateAddresses;
 
+    /// @notice Emitted when a new LifeEstateNFT contract is deployed.
+    /// @param propertyAddress The address of the deployed LifeEstateNFT contract.
+    /// @param propertyName The name of the property for the deployed contract.
     event LifeEstateDeployed(
         address indexed propertyAddress,
         string propertyName
     );
 
+    /// @notice Initializes the contract setting the deployer as the initial owner.
     constructor() Ownable(msg.sender) {}
 
+    /// @notice Deploys a new LifeEstateNFT contract with the provided property details.
+    /// @param _propertyName The name of the property.
+    /// @param _marketPrice The market price of the property.
+    /// @param _propertySurfaceInSquareMeters The surface area of the property in square meters.
+    /// @param _rooms The number of rooms in the property.
+    /// @param _bedRooms The number of bedrooms in the property.
+    /// @param _cityLocation The city where the property is located.
+    /// @param _pool Indicates if the property has a pool.
+    /// @param _garage Indicates if the property has a garage.
+    /// @param _garden Indicates if the property has a garden.
+    /// @param _uri The URI for the metadata of the property.
+    /// @return The address of the newly deployed LifeEstateNFT contract.
     function deployLifeEstate(
         string memory _propertyName,
         uint256 _marketPrice,
@@ -46,6 +65,10 @@ contract LifeEstateFactory is Ownable {
         return address(newLifeEstate);
     }
 
+    /// @notice Initializes the parts of a LifeEstateNFT contract.
+    /// @param newLifeEstate The address of the LifeEstateNFT to initialize.
+    /// @param _partTotalSupplies An array of total supplies for each part.
+    /// @param _partPrices An array of prices for each part.
     function initLifeEstateParts(
         address newLifeEstate,
         uint256[] memory _partTotalSupplies,
@@ -55,12 +78,16 @@ contract LifeEstateFactory is Ownable {
         newNFT.setPartDetails(_partTotalSupplies, _partPrices);
     }
 
-    //for get all properties
+    /// @notice Retrieves all LifeEstateNFT addresses deployed by this factory.
+    /// @return array of addresses of all deployed LifeEstateNFT contracts.
     function getAllLifeEstate() public view returns (address[] memory) {
         return LifeEstateAddresses;
     }
 
-    //for get only one property
+    /// @notice Retrieves a single LifeEstateNFT address by index.
+    /// @param _index The index of the LifeEstateNFT in the `LifeEstateAddresses` array.
+    /// @return address of the LifeEstateNFT contract at the given index.
+    /// @dev Reverts if the index is out of bounds of the `LifeEstateAddresses` array.
     function getOneLifeEstate(uint256 _index) public view returns (address) {
         require(_index < LifeEstateAddresses.length, "Index out of bounds");
         return LifeEstateAddresses[_index];
